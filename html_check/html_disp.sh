134f3e7f2e09bbdae6c0ffe7a6efb48b6e0beaab
SAN_INV <y9_mochi@yahoo.co.jp>
2024-09-28 16:43:33 +0900

    SAN_INV commit 2024/09/28 16:43:33

--- html_check/html_disp.sh
+++ html_check/html_disp.sh
@@ -1,12 +1,1 @@
-#!/usr/bin/env bash
-
-read_link_com=`readlink -f .`
-html_dir="http://localhost:8080${read_link_com##*www}"
-
-echo -e " ※ 以下のURLでPHP動作確認ができます。\n"
-for file_name in `find . -type f | grep -e \.php$ -e \.html$ -e \.htm$ | sed -e "s/^\.//g"`
-do
-   echo ${html_dir}${file_name}
-done
-
-
+../html_disp.sh
--- html_disp.sh
+++ html_disp.sh
@@ -4,7 +4,9 @@ read_link_com=`readlink -f .`
 html_dir="http://localhost:8080${read_link_com##*www}"
 
 echo -e " ※ 以下のURLでPHP動作確認ができます。\n"
-for file_name in `find . -type f | grep -e \.php$ -e \.html$ -e \.htm$ | sed -e "s/^\.//g"`
+for file_name in `find . -type f          | \
+    grep -e \.php$ -e \.html$ -e \.htm$   | \
+    sed -e "s/^\.//g"`
 do
    echo ${html_dir}${file_name}
 done
